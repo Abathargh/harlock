ff5aa1ebf47d9c59546c19631778b84080777ff1
mar <gim.marcello@gmail.com>
2022-01-08 14:34:06 +0100

    changed: added support for multi-line expressions in the repl.

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/Abathargh/harlock/internal/evaluator"
 	"github.com/Abathargh/harlock/internal/lexer"
@@ -12,33 +13,67 @@ import (
 	"github.com/Abathargh/harlock/internal/parser"
 )
 
-const PROMPT = ">> "
+const PROMPT = ">>> "
+const FOLLOWING = "... "
 
 func Start(input io.Reader, output io.Writer) {
 	scanner := bufio.NewScanner(input)
 	env := object.NewEnvironment()
 
+	var buf strings.Builder
+	exprStarted := false
+
 	for {
-		_, _ = fmt.Fprintf(output, PROMPT)
+		if !exprStarted {
+			_, _ = fmt.Fprintf(output, PROMPT)
+		} else {
+			_, _ = fmt.Fprintf(output, FOLLOWING)
+		}
 		if !scanner.Scan() {
 			return
 		}
 
 		line := scanner.Text()
-		l := lexer.NewLexer(bufio.NewReader(bytes.NewBufferString(line)))
-		p := parser.NewParser(l)
-		program := p.ParseProgram()
-		if len(p.Errors()) != 0 {
-			printParserErrors(output, p.Errors())
-			continue
-		}
 
-		evaluatedProg := evaluator.Eval(program, env)
-		if evaluatedProg != nil {
-			_, _ = io.WriteString(output, evaluatedProg.Inspect())
-			_, _ = io.WriteString(output, "\n")
+		switch {
+		case line == "" && !exprStarted:
+			continue
+		case line == "" && exprStarted:
+			exprStarted = false
+			if !parseAndEval(output, buf.String(), env) {
+				buf.Reset()
+				continue
+			}
+			buf.Reset()
+		case line != "" && !exprStarted:
+			if !strings.HasSuffix(line, "{") {
+				parseAndEval(output, line, env)
+				continue
+			}
+			exprStarted = true
+			fallthrough
+		case line != "" && exprStarted:
+			buf.WriteString(line)
+			buf.WriteString("\n")
 		}
 	}
+}
+
+func parseAndEval(output io.Writer, input string, env *object.Environment) bool {
+	l := lexer.NewLexer(bufio.NewReader(bytes.NewBufferString(input)))
+	p := parser.NewParser(l)
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(output, p.Errors())
+		return false
+	}
+
+	evaluatedProg := evaluator.Eval(program, env)
+	if evaluatedProg != nil {
+		_, _ = io.WriteString(output, evaluatedProg.Inspect())
+		_, _ = io.WriteString(output, "\n")
+	}
+	return true
 }
 
 func printParserErrors(writer io.Writer, errors []string) {
