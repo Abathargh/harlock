8664805710fa7ed69cdea29cb7129b733ed63664
mar <gim.marcello@gmail.com>
2022-02-11 23:41:31 +0100

    changed: added a FileError type and a CustomError function within the errors.go file.

--- internal/evaluator/hex/errors.go
+++ internal/evaluator/hex/errors.go
@@ -1,5 +1,7 @@
 package hex
 
+import "fmt"
+
 // RecordError identifies an error related to a hex record
 type RecordError string
 
@@ -14,4 +16,25 @@ const (
 	DataOutOfBounds      = RecordError("the passed byte slice cannot be held by this record")
 	InvalidHexDigit      = RecordError("the passed byte represents a character that is not an hex digit")
 	NoMoreRecordsErr     = RecordError("no more records")
+)
+
+// FileError identifies an error related to a hex file
+type FileError string
+
+// Error returns a string representation of a FileError
+func (r FileError) Error() string {
+	return string(r)
+}
+
+// CustomError returns FileError that can use the classic fmt message/varargs.
+func CustomError(original FileError, msg string, args ...any) FileError {
+	augArgs := []any{original, msg}
+	augArgs = append(augArgs, args...)
+	return FileError(fmt.Sprintf("%w: %s", augArgs...))
+}
+
+const (
+	MultipleEofErr    = FileError("the passed hex file contains more than one EOF records")
+	NoEofRecordErr    = FileError("the passed hex file does not contain an EOF record")
+	AccessOutOfBounds = FileError("cannot access the hex file out of the length of the encoded program")
 )
