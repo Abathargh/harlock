73054a916a1b1ee8fef6034ab52f1618342f68e8
mar <gim.marcello@gmail.com>
2022-02-12 22:59:58 +0100

    changed: added test cases for the WriteAt operation.

--- internal/evaluator/hex/hex_test.go
+++ internal/evaluator/hex/hex_test.go
@@ -53,10 +53,6 @@ func TestReadAll(t *testing.T) {
 				t.Fatalf("unexpected error: %s", err)
 			}
 
-			if file.base != 0 {
-				t.Fatalf("expected default base = 0, got %d", file.base)
-			}
-
 			if len(file.records) != expected {
 				t.Fatalf("expected %q records, got %q", expected, len(file.records))
 			}
@@ -137,5 +133,88 @@ func TestFile_ReadAt(t *testing.T) {
 			}
 		}
 	}
+}
 
+func TestFile_WriteAt(t *testing.T) {
+	hexFile := `:020000021000EC
+:10C20000E0A5E6F6FDFFE0AEE00FE6FCFDFFE6FD93
+:10C21000FFFFF6F50EFE4B66F2FA0CFEF2F40EFE90
+:10C22000F04EF05FF06CF07DCA0050C2F086F097DF
+:10C23000F04AF054BCF5204830592D02E018BB03F9
+:020000022000DC
+:04000000FA00000200
+:00000001FF
+`
+	tests := []struct {
+		pos           uint32
+		input         []byte
+		expectedError error
+	}{
+		{0, []byte{}, nil},
+		{
+			0x1000*16 + 0xC200,
+			[]byte{0x00, 0xEE, 0xAE, 0xBC, 0x01, 0x02, 0x03, 0x04, 0xCC, 0x05, 0x60, 0x71, 0x44, 0x12, 0xF7, 0xA1},
+			nil,
+		},
+		{
+			0x1000*16 + 0xC200,
+			[]byte{0xAA, 0xBD, 0x1C},
+			nil,
+		},
+		{
+			0x1000*16 + 0xC200,
+			[]byte{0x00, 0xEE, 0xAE, 0xBC, 0x01, 0x02, 0x03, 0x04, 0xCC, 0x05, 0x60, 0x71, 0x44, 0x12, 0xF7, 0xA1, 0xFF, 0xFD},
+			nil,
+		},
+		{
+			0x1000*16 + 0xC202,
+			[]byte{0x00, 0xEE, 0xAE, 0xBC, 0x01, 0x02, 0x03, 0x04, 0xCC, 0x05, 0x60, 0x71, 0x44, 0x12, 0xF7, 0xA1},
+			nil,
+		},
+		{
+			0x1000*16 + 0xC202,
+			[]byte{0x00, 0xEE, 0xAE, 0xBC, 0x01, 0x02, 0x03, 0x04, 0xCC, 0x05, 0x60, 0x71, 0x44, 0x12, 0xF7, 0xA1, 0x01, 0x09, 0x21, 0x23},
+			nil,
+		},
+		{
+			0x2000*16 - 2,
+			[]byte{0xAA, 0xBD, 0x1C, 0x2C},
+			AccessOutOfBounds,
+		},
+		{
+			0x2000 * 16,
+			[]byte{0xAA, 0xBD, 0x1C, 0x2C, 0x00, 0xFE},
+			AccessOutOfBounds,
+		},
+		{
+			0x2000 * 16,
+			[]byte{0xAA, 0xBD, 0x1C, 0x2C},
+			nil,
+		},
+	}
+
+	for _, testCase := range tests {
+		file, _ := ReadAll(bytes.NewBufferString(hexFile))
+		err := file.WriteAt(testCase.pos, testCase.input)
+
+		switch testCase.expectedError {
+		case AccessOutOfBounds:
+			if !errors.Is(err, testCase.expectedError) {
+				t.Errorf("expected %q error, got %v", testCase.expectedError, err)
+			}
+		case nil:
+			if err != nil {
+				t.Fatalf("unexpected error: %s, expecting %v", err, testCase.expectedError)
+			}
+
+			readData, err := file.ReadAt(testCase.pos, len(testCase.input))
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			if !reflect.DeepEqual(readData, testCase.input) {
+				t.Errorf("expected read data @%X to be %v, got %v", testCase.pos, testCase.input, readData)
+			}
+		}
+	}
 }
