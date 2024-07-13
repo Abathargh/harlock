010465ec768b87ffd823d9f88cbc5b8ce3c27d7e
mar <gim.marcello@gmail.com>
2024-07-13 12:44:10 +0200

    fixed: drop support for building darwin/386 and darwin/arm binaries, since they are not supported anymore for go 1.15+. Fix build script not stopping on errors.

--- scripts/build-standalone.sh
+++ scripts/build-standalone.sh
@@ -3,20 +3,26 @@
 version=$(git describe --tags)
 vers_nov=${version#*v}
 
-target_archs=(386 amd64 arm64)
+target_archs=(386 amd64 arm arm64)
 target_oss=(linux darwin windows)
 
 mkdir -p dist
 
 for os in "${target_oss[@]}"; do
   for arch in "${target_archs[@]}"; do
-    GOARCH="$arch" GOOS="$os" make build
+    if [ "$os" = "darwin" ]; then
+     if [ "$arch" = "386" ] || [ "$arch" = "arm" ]; then
+      # darwin/386 and darwin/arm support has been dropped since go1.15
+      continue
+      fi
+    fi
+    GOARCH="$arch" GOOS="$os" make build || exit 1
     arname="harlock_${vers_nov}_${os}_${arch}"
     if [ "$os" = "windows" ]; then
-      zip "$arname".zip README.md LICENSE harlock.exe
+      zip "$arname".zip README.md LICENSE harlock.exe || exit 1
       mv "$arname.zip" dist/
     else
-      tar -czvf "$arname".tar.gz README.md LICENSE harlock
+      tar -czvf "$arname".tar.gz README.md LICENSE harlock || exit 1
       mv "$arname.tar.gz" dist/
     fi
   done
